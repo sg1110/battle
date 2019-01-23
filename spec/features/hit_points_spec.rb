cdaf7443e07db257cbc408961c78b75f751904bd
sg1110 <saule.guzyte@tesco.com>
2019-01-23 14:26:28 +0000

    Web helper added to shorten tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit point' do
-    visit ('/')
-    fill_in :player_1_name, with: 'Cat'
-    fill_in :player_2_name, with: 'Dog'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Dog: 60HP'
   end
 end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter player names' do
   scenario "Can receive entered player names" do
-    visit ('/')
-    fill_in :player_1_name, with: 'Cat'
-    fill_in :player_2_name, with: 'Dog'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Cat vs. Dog'
   end
 end
