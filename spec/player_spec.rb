0359ab6ad0bdc35c63f6b489a594349f4c17e2ea
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-23 15:33:56 +0000

    Added player class which stores its name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,8 @@
-# require 'player'
-#
-# describe Player do
-#
-# end
+require 'player'
+
+describe Player do
+  let(:player_1) { Player.new('Cat') }
+  it 'has a name' do
+    expect(player_1.name).to eq 'Cat'
+  end
+end
