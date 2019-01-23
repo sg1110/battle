14c78becfd7a47d03f9853633d113abfa2676316
sg1110 <saule.guzyte@tesco.com>
2019-01-23 16:57:45 +0000

    Game class accepts two Player instances,nr. of global variables reduced

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,21 @@
 require 'game'
 
 describe Game do
-  let(:dog) { double(:player, receives_damage: true) }
+  let(:dog) {double(:player, receives_damage: true) }
+  let(:cat) {double(:player)}
+  let(:game) {Game.new(cat,dog)}
 
   it 'can attack a target' do
     expect(dog).to receive(:receives_damage)
-    subject.attack(dog)
+    game.attack(dog)
   end
+
+  it 'Game initializes with player 1 object' do
+    expect(game.player_1).to eq cat
+  end
+
+  it 'Game initializes with player 2 object' do
+    expect(game.player_2).to eq dog
+  end
+
 end
