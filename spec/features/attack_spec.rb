80a1bc78b2b5b9228863e296f78712d49934cf8e
sg1110 <saule.guzyte@tesco.com>
2019-01-24 14:22:48 +0000

    Multiplayer functionality implemented and tests working

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,4 +1,4 @@
-feature 'Player 1 attack player 2' do
+feature 'Attack and reduce points' do
   scenario "Can reduce player 2 HP" do
     sign_in_and_play
     click_button 'Attack'
@@ -10,5 +10,11 @@ feature 'Player 1 attack player 2' do
     click_button 'Attack'
     click_button 'Ok'
     expect(page).to have_content "Dog: 90HP"
+  end
+
+  scenario 'Multiple attacks reduce both scores' do
+    sign_in_and_play
+    3.times { attack_and_confirm }
+    expect(page).to have_content "Dog: 80HP"
   end
 end
